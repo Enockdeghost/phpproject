d6ba3c14d2610cc1eb0f566f115bd0dda5d7fb95
enockdeghost <enockdeghost@gmil.com>
2026-02-17 16:01:08 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+  $marks = 30;
+
+  if ($marks > 30 ){
+    echo 'hello world';
+  }
+   $name = ['enock','stepani',34,53,54];
+
+   print_r($name); // if you want stucture
+    echo implode(", ", $name);// use if you want 
+    var_dump($name); // if you want debugn info
+?>
