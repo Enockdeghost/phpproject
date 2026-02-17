1214cbe3acd9686a5713c79a134a5d594d6989bc
enockdeghost <enockdeghost@gmil.com>
2026-02-17 16:07:42 +0300

    variable

--- day1.php
+++ day1.php
@@ -1,3 +1,15 @@
 <?php
   echo "hello world";
+
+
+  // variables in php
+  $name = "enock"; //for string
+  $age = 34; //for integer
+  $is_student = true; //for boolean
+  $is_float = 3.14; //for float
+  echo "my name is $name and iam $age years old"; //string interpolation
 ?>
+
+
+
+
