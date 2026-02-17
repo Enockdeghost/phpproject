bdae5da9f8ad5cd79f6f25980bb41397ca358473
enockdeghost <enockdeghost@gmil.com>
2026-02-17 16:19:39 +0300

    mplement age-based messaging logic in if_else.php

--- if_else.php
+++ if_else.php
@@ -1,3 +1,14 @@
 <?php
-    
+    $age = 20; //variable
+
+    if ($age >= 18) { //if age is equal or greater than 18
+        echo "your age is $age and still young to enjoy life";
+    }elseif ($age >= 25) {
+        echo "your age is $age and you are in strengh age";
+    }elseif ($age >=30) {
+        echo "your age is $age you must have a family";
+    }
+    else {
+        echo "yo waiting to die";
+    }
 ?>
